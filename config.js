bff4923cd46322e69151f8c5e8bfc96602d918f1
Charlie999yt <88020783+Charlie999yt@users.noreply.github.com>
2021-11-09 20:32:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,27 +2,27 @@ let fs = require('fs')
 
 userbot = {
  Prem: JSON.parse(fs.readFileSync('./db/premium.json')),
- owner: [ "6281261324817", "6281916259786", "62857420881846","628812904283","6282328303332", "62822980698995", "436504463151666", "62823283033323" ],
+ owner: [ "919556416715", "6281916259786", "62857420881846","628812904283","6282328303332", "62822980698995", "436504463151666", "62823283033323" ],
   MONGO_URI: "mongodb+srv://<username>:<password>@cluster0.eyx0e.mongodb.net/<dbname>?retryWrites=true&w=majority",
    mess: {
-   wait: "tunggu sedang di proses...",
+   wait: "wait in proses...",
    error: "errorr!!"
    },
    prefix: "_",
    limit: 100,
-   eror: "Sepertinya Eror",
-   waits: "Sedang di proses",
+   eror: "Eror",
+   waits: "wait in proses",
    butmag: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTHDLrd0_DwHnJfuZ_pRMOAbLi3-ot-NxyPA&usqp=CAU', // buat Image Button
-   packname: "RIXLE-BOT OFFICIAL",
-   author: '© 2021 - FearTeam',
+   packname: "BOT OFFICIAL",
+   author: '© 2021 - indianTeam',
     setting: {
     admin: "only admin",
     group: "only group",
     owner: "owner only pack",
     jadibot: "jadibot only",
-    botadmin: "bot harus menjadi admin",
-    userPlayer: "Anda Belum Daftar Dalam DB Gamer RIXLE-BOT", 
-    nsfw: "FITUR BELUM DIAKTIFKAN!"
+    botadmin: "bot must be admin",
+    userPlayer: "You Haven't Registered In Gamer DB RIXLE-BOT", 
+    nsfw: "FITUR NOT ACTIVATED!"
     }
 }
 module.exports = userbot
